fc47e73ba76e55b4a9741206ffdc76aaa38450f4
Jerryaa98 <jerryabuayob@gmail.com>
2025-10-27 15:18:49 +0200

    modoified run_test

--- run_test.py
+++ run_test.py
@@ -9,10 +9,52 @@ from core.config import Config
 from core import Test
 
 
-PATH = "/root/LibFewShot/results/Baseline-KOS_1_alpha_spec-Conv64F-5-1-Oct-04-2025-11-58-31"
+PATH = "/root/LibFewShot/results/DeepBDC-KOS_0.2_alpha_spec-resnet12Bdc-5-1-Oct-23-2025-16-57-23"
+
+# deepbdc 1shot iid seed 0
+# PATH = "/root/LibFewShot/results/DeepBDC-KOS_1_alpha_spec-resnet12Bdc-5-1-Oct-09-2025-01-42-22"
+# deepbdc 5shot iid seed 0
+# PATH = "/root/LibFewShot/results/DeepBDC-KOS_1_alpha_spec-resnet12Bdc-5-5-Oct-09-2025-01-41-51"
+# # deepbdc 1shot ood seed 0
+# PATH = "/root/LibFewShot/results/DeepBDC-KOS_1_alpha_spec-resnet12Bdc-5-1-Oct-09-2025-01-42-22"
+# # deepbdc 5shot ood seed 0
+# PATH = "/root/LibFewShot/results/DeepBDC-KOS_1_alpha_spec-resnet12Bdc-5-5-Oct-09-2025-01-52-14"
+# deepbdc 10 shot iid seed 1
+# PATH = "/root/LibFewShot/results/DeepBDC-KOS_1_alpha_spec-resnet12Bdc-5-10-Oct-13-2025-14-55-38"
+# deepbdc 10 shot ood seed 1
+# PATH = "./results/DeepBDC-KOS_1_alpha_spec-resnet12Bdc-5-10-Oct-13-2025-20-27-58"
+
+# meta baseline 1 shot iid seed 0
+# PATH = "/root/LibFewShot/results/MetaBaseline-KOS_1_alpha_spec-Conv64F-5-1-Oct-09-2025-01-20-49"
+# meta baseline 1 shot ood seed 0
+# PATH = "/root/LibFewShot/results/MetaBaseline-KOS_1_alpha_spec-Conv64F-5-1-Oct-09-2025-01-23-50"
+# meta baseline 5 shot iid seed 0
+# PATH = "/root/LibFewShot/results/MetaBaseline-KOS_1_alpha_spec-Conv64F-5-5-Oct-09-2025-01-21-49"
+# meta baseline 5 shot ood seed 0
+# PATH = "/root/LibFewShot/results/MetaBaseline-KOS_1_alpha_spec-Conv64F-5-5-Oct-09-2025-01-21-48"
+# meta baseline 10 shot iid seed 0
+# PATH = "/root/LibFewShot/results/MetaBaseline-KOS_1_alpha_spec-Conv64F-5-10-Oct-13-2025-05-31-40"
+# meta baseline 10 shot ood seed 0
+# PATH = "/root/LibFewShot/results/MetaBaseline-KOS_1_alpha_spec-Conv64F-5-10-Oct-13-2025-06-58-35"
+
+# r2d2 1 shot iid seed 0
+# PATH = "/root/LibFewShot/results/R2D2-KOS_1_alpha_spec-Conv64F-5-1-Oct-05-2025-05-04-17"
+# r2d2 1 shot ood seed 0
+# PATH = "/root/LibFewShot/results/R2D2-KOS_1_alpha_spec-Conv64F-5-1-Oct-05-2025-18-05-51"
+# r2d2 5 shot iid seed 0
+# PATH = "/root/LibFewShot/results/R2D2-KOS_1_alpha_spec-Conv64F-5-5-Oct-05-2025-11-37-37"
+# r2d2 5 shot ood seed 0
+# PATH = "/root/LibFewShot/results/R2D2-KOS_1_alpha_spec-Conv64F-5-5-Oct-06-2025-14-30-33"
+# r2d2 10 shot iid seed 0
+# PATH = "/root/LibFewShot/results/R2D2-KOS_1_alpha_spec-Conv64F-5-10-Oct-13-2025-15-22-56"
+# r2d2 10 shot ood seed 0
+# PATH = "/root/LibFewShot/results/R2D2-KOS_1_alpha_spec-Conv64F-5-10-Oct-14-2025-00-44-35"
+
+
+
 VAR_DICT = {
     "test_epoch": 1,
-    "test_episode": 1000
+    "test_episode": 400
 }
 
 
